a963a9b61eb689e977209af0b19e2a204aa46577
shabdi08 <shabdisrivastava@gmail.com>
2025-08-31 22:47:48 +0530

    Create experiment 1.1.c

---
+++ experiment 1.1.c
@@ -0,0 +1,16 @@
+/* Experiment 1: Installation, Environment Setup,  
+and starting with the C language.  
+2) ⁠ Write a C Program to print the address in multiple lines*/ 
+#include <stdio.h>  
+int main() 
+{  
+printf ("Name- Shabdi Srivastava\nSAP_ID-590021135\nCourse-BCA\nBatch-6");  
+printf("\n--------------------------------------\n");  
+printf("House No: 356/k-103 \n");  
+printf("Street  : rajajipuram\n");  
+printf("City    
+: Lucknnow\n");  
+printf("State   : Uttar Pradesh\n");  
+printf("Pincode : 226017\n");  
+return 0;  
+} 
